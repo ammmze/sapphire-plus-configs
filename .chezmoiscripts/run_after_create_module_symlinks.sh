93082a850d02e51af69f7af7ea6593acfaf89c7b
Branden Cash <203336+ammmze@users.noreply.github.com>
2023-02-23 22:33:13 -0700

    fix: change config.next from being symlink to being copied

--- .chezmoiscripts/run_after_create_module_symlinks.sh
+++ .chezmoiscripts/run_after_create_module_symlinks.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+KLIPPER_PATH="${HOME}/klipper"
+PRINTER_CONFIG_PATH="${HOME}/printer_data/config.next"
+
 # ln -sf ~/jschuh-klipper-macros ./config.next/modules.d/km
-ln -sf ~/Klipper-Adaptive-Meshing-Purging/Configuration ./printer_data/config.next/modules.d/KAMP
+# ln -sfT ~/Klipper-Adaptive-Meshing-Purging/Configuration "${PRINTER_CONFIG_PATH}/modules.d/KAMP"
+# ln -sfT ~/klipper-led_effect/led_effect.py "${KLIPPER_PATH}/klippy/extras/led_effect.py"
