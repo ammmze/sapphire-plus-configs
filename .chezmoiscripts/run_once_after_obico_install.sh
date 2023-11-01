2371bb257036b56933a3a0dbe5d628eb51aec61f
Branden Cash <203336+ammmze@users.noreply.github.com>
2023-11-01 01:24:20 -0700

    feat(obico): set install parameters

--- .chezmoiscripts/run_once_after_obico_install.sh
+++ .chezmoiscripts/run_once_after_obico_install.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 cd ~/obico
-./install.sh
+./install.sh \
+    -H $(hostname).packnet.cc \
+    -p 80 \
+    -C "${HOME}/printer_data/config/moonraker.conf" \
+    -l "${HOME}/printer_data/logs/moonraker-obico-80.log" \
+    -S https://obico.cashmedia.me
