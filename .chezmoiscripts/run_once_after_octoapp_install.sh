cb395c34acf1484994239d2657aac9039a1417e7
Branden Cash <203336+ammmze@users.noreply.github.com>
2023-12-24 00:59:53 -0700

    feat: add katapult/canboot

--- .chezmoiscripts/run_once_after_octoapp_install.sh
+++ .chezmoiscripts/run_once_after_octoapp_install.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 /bin/bash -c "$(curl -fsSL https://octoapp.eu/install.sh)"
+
+# TODO: ^ this didn't really work well...it cloned, but actual install didn't go correctly
+# Probably should just use chezmoiexternal to clone it and run the installer ourselves like this:
+# ./install.sh ~/printer_data/config/moonraker.conf moonraker.service
