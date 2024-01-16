67e9dbcb2402ba987f9da38e9baf69aa28e342fc
Branden Cash <203336+ammmze@users.noreply.github.com>
2024-01-15 21:53:52 -0700

    fix: even out quotes

--- .chezmoiscripts/run_once_after_belay_install.sh
+++ .chezmoiscripts/run_once_after_belay_install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 install_url=https://raw.githubusercontent.com/Annex-Engineering/Belay/main/Klipper_Stuff/klippy_module/install.sh
-bash <(curl --fail --silent --show-error "${install_url})
+bash <(curl --fail --silent --show-error "${install_url}")
